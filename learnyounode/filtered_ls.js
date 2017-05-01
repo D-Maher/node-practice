5e94d2921dc37d7d926515ada5b88d84fc0ebe17
Danny Maher <dmaher724@gmail.com>
2017-05-01 16:42:29 -0500

    Refactor list filter program

--- learnyounode/filtered_ls.js
+++ learnyounode/filtered_ls.js
@@ -1,15 +1,14 @@
 var fs = require('fs');
+var path = require('path');
+
 var directory = process.argv[2];
-var ext = process.argv[3];
+var ext = '.' + process.argv[3];
 
 fs.readdir(directory, 'utf8', function filterList(err, list) {
-  if (err) {
-    console.log(err);
-  }
-
-  for (var i = 0; i < list.length; i++) {
-    if (list[i].search("." + ext) > 0) {
-      console.log(list[i]);
+  if (err) return console.log(err);
+  list.forEach(function(file) {
+    if (path.extname(file) === ext) {
+      console.log(file);
     }
-  }
-})
+  });
+});
