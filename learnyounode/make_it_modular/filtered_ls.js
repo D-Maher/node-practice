6a5dee164492ae16a60f67cd085c6147686c5b65
Danny Maher <dmaher724@gmail.com>
2017-05-02 13:17:33 -0500

    Add helpful comments to files in make_it_modular exercise

--- learnyounode/make_it_modular/filtered_ls.js
+++ learnyounode/make_it_modular/filtered_ls.js
@@ -3,10 +3,10 @@ var filterList = require('./my_module');
 var directory = process.argv[2];
 var filter = process.argv[3];
 
-filterList(directory, filter, function (err, list) {
-  if (err) return console.error('There was an error:', err);
+filterList(directory, filter, function (err, list) { // this third argument is the 'callback' function
+  if (err) return console.error('There was an error:', err); // i.e. if there is an error from the 'fs.readdir' function, immediately log an error to the console (early return)
   
   list.forEach(function (file) {
     console.log(file);
-  })
+  }) // since there was no error passed to the callback function, each of the items in the filtered list are printed to the console
 });
