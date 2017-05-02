bcd59260fbe38aba0ab17f45cbf18877ec4fee86
Danny Maher <dmaher724@gmail.com>
2017-05-02 14:06:57 -0500

    Refactor GET request program, add error handling, add helpful comments

--- learnyounode/http_client/http_client.js
+++ learnyounode/http_client/http_client.js
@@ -1,9 +1,17 @@
 var http = require('http');
 
-var url = process.argv[2];
+http.get(process.argv[2], function (response) {
+  response.setEncoding('utf8');
+  response.on('data', console.log);
+  response.on('error', console.error);
+}).on('error', console.error)
 
-http.get(url, function (response) {
-  response.setEncoding('utf8').on("data", function (data) {
-    console.log(data);
-  })
-})
+
+// lines 5 and 6 are shorthand for:
+
+  // response.on('data', function (data) {
+  //   console.log(data);
+  // });
+  // response.on('error', function (error) {
+  //   console.error(error);
+  // });
