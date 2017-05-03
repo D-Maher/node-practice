d83c55e37514dc66439d89d7c0e20cb9c5715a33
Danny Maher <dmaher724@gmail.com>
2017-05-03 02:54:20 -0500

    Adjust HTTP server to send a response header along with the text file

--- learnyounode/http_file_server/http_file_server.js
+++ learnyounode/http_file_server/http_file_server.js
@@ -2,6 +2,9 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function (request, response) {
+  response.writeHead(200, {
+    'content-type': 'text/plain'
+  });
   fs.createReadStream(process.argv[3]).pipe(response);
 });
 
