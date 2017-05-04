3769530629b9242e734247d8992c1d4b504db609
Danny Maher <dmaher724@gmail.com>
2017-05-04 15:49:12 -0500

    Refactor even/odd case transform program

--- stream-adventure/lines/lines.js
+++ stream-adventure/lines/lines.js
@@ -3,14 +3,11 @@ var split = require('split');
 
 var lineCounter = 1;
 
-process.stdin.pipe(split()).pipe(through(function (line, encoding, next) {
-  if (lineCounter % 2 === 0) {
-    console.log(line.toString().toUpperCase());
-  } else {
-    console.log(line.toString().toLowerCase());
-  }
-
-  lineCounter += 1;
-
+var transform = through(function (buffer, encoding, next) {
+  var line = buffer.toString();
+  this.push(lineCounter % 2 === 0 ? line.toUpperCase() + '\n' : line.toLowerCase() + '\n');
+  lineCounter++;
   next();
-})).pipe(process.stdout);
+})
+
+process.stdin.pipe(split()).pipe(transform).pipe(process.stdout);
