141c23f8e7296092b4c9cf4d3a75e2f98bbfe214
Danny Maher <dmaher724@gmail.com>
2017-05-04 13:02:15 -0500

    Refactor transform stream program to remove redundant code

--- stream-adventure/transform/transform.js
+++ stream-adventure/transform/transform.js
@@ -2,15 +2,9 @@
 
 var through = require('through2');
 
-var transformStream = through(write, end); // creates a through stream
-
-function write(buffer, encoding, next) {
-  this.push(buffer.toString().toUpperCase()); // produces output data
-  next(); // receives the next chunk of data
-}
-
-function end(done) { // this is optional
-  done();
-}
+var transformStream = through(function (buffer, encoding, next) {
+  this.push(buffer.toString().toUpperCase());
+  next();
+});
 
 process.stdin.pipe(transformStream).pipe(process.stdout);
